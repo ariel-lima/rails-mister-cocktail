9ec0d2dbb7de9865d827ffdac6e00890dbd9b806
Amanda Perino <amandabperino@gmail.com>
2023-01-23 13:13:35 +0100

    Creating cocktail controller, views, and routes

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,34 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: %i[ create show ]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail), notice: "Cocktail was successfully created."
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:name).permit(:dose, :ingredient)
+  end
+
+end
