fb107fb32b0034356ccc54904bf31eb0b6d66eb6
Amanda Perino <amandabperino@gmail.com>
2023-01-30 15:12:23 +0100

    Doses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root to: "cocktails#home"
-  resources :cocktails, only: [%i[show new create index]]
-  resources :doses, only: [%i[new create destroy]]
+  resources :cocktails, only: [:show, :new, :create, :index]
+  resources :doses, only: [:new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :cocktails
 end
