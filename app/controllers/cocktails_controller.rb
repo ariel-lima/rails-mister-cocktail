e822217e372707293140d55d9d80b7f45e9a844d
Ariel Lima <ariel_lima_oliveira@yahoo.com.br>
2023-01-30 13:24:10 +0000

    doses controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,22 +1,31 @@
 class CocktailsController < ApplicationController
-  # before_action :set_cocktail, only: %i[create show ]
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
 
   def index
     @cocktails = Cocktail.all
-  end
-
-  def new
-    @cocktail = Cocktail.new
   end
 
   def show
     @cocktail = Cocktail.find(params[:id])
   end
 
+  def new
+    @cocktail = Cocktail.new
+  end
+
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
     redirect_to cocktail_path(@cocktail), notice: "Cocktail was successfully created."
+  end
+
+  def edit
+    @doses = @cocktail.doses
+  end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to edit_cocktail_path(@cocktail)
   end
 
   private
@@ -26,7 +35,7 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:name).permit(:dose, :ingredient)
+    params.require(:cocktail).permit(:name, :dose_id, :ingredient_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: "cocktails#home"
-  resources :cocktails, only: [:show, :new, :create, :index]
-  resources :doses, only: [:new, :create, :destroy]
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :doses, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
